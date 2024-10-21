23f6fd4e860f1a635db7f744eb6d602e6f19d854
Zzzas <juanpmc7722@gmail.com>
2024-10-20 23:16:25 -0500

    Contiene atributos como nombre, color y los metodos.

---
+++ src/main/java/com/mycompany/figurasgeometricas/FiguraGeometrica.java
@@ -0,0 +1,35 @@
+package com.mycompany.figurasgeometricas;
+
+public class FiguraGeometrica {
+    private String nombre;
+    private String color;
+
+    public FiguraGeometrica(String nombre, String color) {
+        this.nombre = nombre;
+        this.color = color;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public double obtenerArea() {
+        return 0.0;
+    }
+
+    public double obtenerPerimetro() {
+        return 0.0;
+    }
+}
